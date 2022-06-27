d157e86b77b74a5ac62b8849aaee05ef4a7f405e
elcuenca3 <elcuenca3@utpl.edu.ec>
2022-06-26 22:31:44 -0500

    actualizacion 26/06/2022

---
+++ prroyectociudad/ordenamiento/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import include, path
+from . import views
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('barrios/', views.barrios, 
+        name='barrios'),
+    path('crear/barrio/', views.crear_barrio, 
+        name='crear_barrio'),
+    path('editar/barrio/<int:id>', views.editar_barrio, 
+        name='editar_barrio'),
+    path('crear/parroquia/', views.crear_parroquia, 
+        name='crear_parroquia'),
+    path('editar/parroquia/<int:id>', views.editar_parroquia, 
+        name='editar_parroquia'),
+]
